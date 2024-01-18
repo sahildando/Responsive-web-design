78e2e8817e4a42db7bc722346b485b8c2bff5233
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-01-18 17:24:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+
+
+  var swiper = new Swiper(".Marvel-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 55000,
+      disableOnInteraction: false,
+    },
+    centredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: { 
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+
+      }
+    }
+  })
+
